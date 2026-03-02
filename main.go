1461a1710b828631f9a77b358d297bb673c2d17c
William Story <william.story.e@gmail.com>
2026-03-01 20:01:18 -0700

    Start basic REPL

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scan := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scan.Scan() {
+			fmt.Println("Error reading input:", scan.Err())
+			continue
+		}
+
+		input := scan.Text()
+		cleanedInput := cleanInput(input)
+
+		if len(cleanedInput) == 0 {
+			fmt.Println("Specify an input")
+		} else {
+			fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
